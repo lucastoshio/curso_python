68f64f068ba3aa58cdbbf6439184026101a3679c
Lucas Toshio <lucasmiki@outlook.com>
2023-06-10 18:37:20 -0300

    novos exercicios

---
+++ Ex_Dicionario.py
@@ -0,0 +1,12 @@
+# Criando um dicionario chamado Contato
+contato = {"@camila": "camila", "@amanda": "amanda", "@fernanda": "fernanda"}
+
+# função keys() retorta a chave
+for insta in contato.keys():
+    print(insta)
+# função values() retorna o valor
+for atriz in contato.values():
+    print(atriz)
+# função items() retorna ambos
+for insta, atriz in contato.items():
+    print(" o instagran de {} é {}".format(atriz, insta))
